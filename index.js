d7ab10c3c0aca3a0205ecadfce96407c732247ed
Adam m <adam@floorsmtihllc.com>
2020-03-07 17:15:52 -0500

    attempt 3 test 15

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 // Creates the endpoint for our webhook 
+app.set("view engine", "ejs");
+
 app.get("/", function(_req, res) {
     res.render("index");
   });
