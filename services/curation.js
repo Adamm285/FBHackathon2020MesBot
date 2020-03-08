d8f6d6ded1bb696e0ba70b5e64dfe5d721f7cc15
Adam m <adam@floorsmtihllc.com>
2020-03-08 17:15:25 -0400

    experiment 1

--- services/curation.js
+++ services/curation.js
@@ -297,8 +297,8 @@ module.exports = class Curation {
     case "CURATION_SWISS_BOTH_WHOLE":
     case "CURATION_SWISS_BOTH_HALF":
     //MAKE THE CARDS OF SUBS
-    response = this.genCurationResponse(payload);
-    break;
+    // response = this.genCurationResponse(payload);
+    // break;
     case "CURATION_OTHER_STYLE":
     // Build the recommendation logic here
     // outfit = "chickentender";${outfit}
