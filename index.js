6a2a68b05c3fc33b65666c7065b33983fb6967a6
Adam m <adam@floorsmtihllc.com>
2020-03-08 15:47:29 -0400

    adding post /webhook form app

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const
     GraphAPi = require("./services/graph-api"),
     User = require("./services/user"),
     config = require("./services/config"),
+    i18n = require("./i18n.config"),
     app = express().use(bodyParser.json()); // creates express http server
     var users = {};
 // Sets server port and logs message on success
@@ -107,30 +108,99 @@ app.get("/profile", (req, res) => {
     }
 });
 // Creates the endpoint for our webhook 
-app.post('/webhook', (req, res) => {
-
+app.post("/webhook", (req, res) => {
     let body = req.body;
-
-    // Checks this is an event from a page subscription
-    if (body.object === 'page') {
-
-        // Iterates over each entry - there may be multiple if batched
-        body.entry.forEach(function (entry) {
-
-            // Gets the message. entry.messaging is an array, but 
-            // will only ever contain one message, so we get index 0
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-        });
-
-        // Returns a '200 OK' response to all requests
-        res.status(200).send('EVENT_RECEIVED');
+  
+    // Checks if this is an event from a page subscription
+    if (body.object === "page") {
+      // Returns a '200 OK' response to all requests
+      res.status(200).send("EVENT_RECEIVED");
+  
+      // Iterates over each entry - there may be multiple if batched
+      body.entry.forEach(function(entry) {
+        if ("changes" in entry) {
+          // Handle Page Changes event
+          let receiveMessage = new Receive();
+          if (entry.changes[0].field === "feed") {
+            let change = entry.changes[0].value;
+            switch (change.item) {
+              case "post":
+                return receiveMessage.handlePrivateReply(
+                  "post_id",
+                  change.post_id
+                );
+                break;
+              case "comment":
+                return receiveMessage.handlePrivateReply(
+                  "comment_id",
+                  change.comment_id
+                );
+                break;
+              default:
+                console.log('Unsupported feed change type.');
+                return;
+            }
+          }
+        }
+  
+        // Gets the body of the webhook event
+        let webhookEvent = entry.messaging[0];
+        // console.log(webhookEvent);
+  
+        // Discard uninteresting events
+        if ("read" in webhookEvent) {
+          // console.log("Got a read event");
+          return;
+        }
+  
+        if ("delivery" in webhookEvent) {
+          // console.log("Got a delivery event");
+          return;
+        }
+  
+        // Get the sender PSID
+        let senderPsid = webhookEvent.sender.id;
+  
+        if (!(senderPsid in users)) {
+          let user = new User(senderPsid);
+  
+          GraphAPi.getUserProfile(senderPsid)
+            .then(userProfile => {
+              user.setProfile(userProfile);
+            })
+            .catch(error => {
+              // The profile is unavailable
+              console.log("Profile is unavailable:", error);
+            })
+            .finally(() => {
+              users[senderPsid] = user;
+              i18n.setLocale(user.locale);
+              console.log(
+                "New Profile PSID:",
+                senderPsid,
+                "with locale:",
+                i18n.getLocale()
+              );
+              let receiveMessage = new Receive(users[senderPsid], webhookEvent);
+              return receiveMessage.handleMessage();
+            });
+        } else {
+          i18n.setLocale(users[senderPsid].locale);
+          console.log(
+            "Profile already exists PSID:",
+            senderPsid,
+            "with locale:",
+            i18n.getLocale()
+          );
+          let receiveMessage = new Receive(users[senderPsid], webhookEvent);
+          return receiveMessage.handleMessage();
+        }
+      });
     } else {
-        // Returns a '404 Not Found' if event is not from a page subscription
-        res.sendStatus(404);
+      // Returns a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
     }
-
-});
+  });
 // 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
