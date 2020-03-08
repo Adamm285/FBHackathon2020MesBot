49d313d3b5f6f3fb301e203bd1bd9e55b47004ae
Adam m <adam@floorsmtihllc.com>
2020-03-08 19:24:13 -0400

    curation test 5

--- services/curation.js
+++ services/curation.js
@@ -44,7 +44,7 @@ module.exports = class Curation {
         break;
         // 
       case "CURATION":
-        outfit = `${this.user.gender}-${this.randomOutfit()}`;
+        // outfit = `${this.user.gender}-${this.randomOutfit()}`;
         response = [
           Response.genText(i18n.__("curation.prompt")),
           Response.genGenericTemplate(
