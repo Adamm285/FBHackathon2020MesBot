be9976010ba1f8673d01e8d3627cf9dbd7fc78c1
Adam m <adam@floorsmtihllc.com>
2020-03-08 12:46:49 -0400

    begining 3/8 test 1

--- services/curation.js
+++ services/curation.js
@@ -27,7 +27,7 @@ module.exports = class Curation {
     let outfit;
     // 
     switch (payload) {
-      case "SUMMER_COUPON":
+      case "CURATION":
         response = [
           Response.genText(
             i18n.__("leadgen.promo", {
@@ -70,35 +70,8 @@ module.exports = class Curation {
         ];
         break;
         // 
-      case "CURATION":
-        response = {
-          "attachment": {
-            "type": "template",
-            "payload": {
-              "template_type": "generic",
-              "elements": [{
-                "title": "Welcome!",
-                "image_url": "https://petersfancybrownhats.com/company_image.png",
-                "subtitle": "We have the right hat for everyone.",
-                "default_action": {
-                  "type": "web_url",
-                  "url": "https://petersfancybrownhats.com/view?item=103",
-                  "webview_height_ratio": "tall",
-                },
-                "buttons": [{
-                  "type": "web_url",
-                  "url": "https://petersfancybrownhats.com",
-                  "title": "View Website"
-                }, {
-                  "type": "postback",
-                  "title": "Start Chatting",
-                  "payload": "DEVELOPER_DEFINED_PAYLOAD"
-                }]
-              }]
-            }
-          }
-        }
-        "https://graph.facebook.com/v2.6/me/messages?access_token=TOKEN"
+      // case "CURATION":
+
     
 
 
