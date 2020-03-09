ccd99ffa866196cb1e9f1c547b0f7e65f86d1084
Adam m <adam@floorsmtihllc.com>
2020-03-09 19:56:43 -0400

    tracing response test 8

--- index.js
+++ index.js
@@ -244,7 +244,7 @@ function callSendAPI(sender_psid, response) {
   // Send the HTTP request to the Messenger Platform
   request({
       "uri": "https://graph.facebook.com/v2.6/me/messages",
-      "qs": {"access_token": PAGE_ACCESS_TOKEN},
+      "qs": {"access_token": process.env.PAGE_ACCESS_TOKEN},
       "method": "POST",
       "json": request_body
   }, (err, res, body) => {
