d7bdd5a9e95d1277a8f7776502829c31f78dc5d2
Adam m <adam@floorsmtihllc.com>
2020-03-14 19:59:16 -0400

    back to adjusting curation.js 2

--- index.js
+++ index.js
@@ -286,7 +286,7 @@ function callSendAPI(sender_psid, response) {
       switch (request_body.message.text.replace(/[^\w\s]/gi, '').trim().toLowerCase()) {
         case request_body.message.text.replace(/[^\w\s]/gi, '').trim().toLowerCase():
           console.log("----------------!");
-          response = Curation.handlePayload();
+          response = Curation.handlePayload(i18n.__("curation.received"));
           console.log("----------------!");
           break;
         default:
