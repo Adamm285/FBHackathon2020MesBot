c1e2fabc7f9a10f56c6fcfe2a92b743e45ff24f5
Adam m <adam@floorsmtihllc.com>
2020-03-14 17:58:30 -0400

    console log response 9

--- services/orderid.js
+++ services/orderid.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 
 const subSchema = new Schema({
   response: { type: String, required: true },
-  payload: { type: String, required: true },
   date: { type: Date, default: Date.now }
 });
 
