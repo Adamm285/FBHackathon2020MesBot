23a24f2b1ebb6543eb02cdf95a0249dcb7ac4835
Adam m <adam@floorsmtihllc.com>
2020-03-14 22:19:01 -0400

    working directions for a reply 4

--- services/directions.js
+++ services/directions.js
@@ -2,86 +2,83 @@
 
 // Imports dependencies
 const Response = require("./response"),
-  i18n = require("../i18n.config"),
-  config = require("./config");
+    i18n = require("../i18n.config"),
+    config = require("./config");
 
 module.exports = class Order {
-  static handlePayload(payload) {
-    let response;
-    console.log("switching to directions...", payload);
-    response = 
-          Response.genQuickReply(i18n.__("directions.received"), [{
-              title: i18n.__("curation.directions"),
-              payload: "CLOSEST_DELI"
-            },
-            {
-              title: i18n.__("curation.sales"),
-              payload: "CARE_SALES"
-            }
-          ]);
-    switch (payload) {
-        
-        case "Add to cart":
-            console.log("switched to directions...")
-        response = 
-          Response.genQuickReply(i18n.__("directions.received"), [{
-              title: i18n.__("curation.directions"),
-              payload: "CLOSEST_DELI"
-            },
-            {
-              title: i18n.__("curation.sales"),
-              payload: "CARE_SALES"
-            }
-          ])
-        
+    static handlePayload(payload) {
+        let response;
+        console.log("switching to directions...", payload);
 
-        break;
-      case "CLOSEST_DELI":
-        response = Response.genPostbackButton(
-          i18n.__("curation.show"),
-          "CURATION_OTHER_STYLE"
-        )
-        break;
-      case "TRACK_ORDER":
-        response = Response.genQuickReply(i18n.__("order.prompt"), [
-          {
-            title: i18n.__("order.account"),
-            payload: "LINK_ORDER"
-          },
-          {
-            title: i18n.__("order.search"),
-            payload: "SEARCH_ORDER"
-          },
-          {
-            title: i18n.__("menu.help"),
-            payload: "CARE_ORDER"
-          }
-        ]);
-        break;
+        switch (payload) {
+            case payload:
+                console.log("switched to directions...")
+                response =
+                    Response.genQuickReply(i18n.__("directions.received"), [{
+                            title: i18n.__("curation.directions"),
+                            payload: "CLOSEST_DELI"
+                        },
+                        {
+                            title: i18n.__("curation.sales"),
+                            payload: "CARE_SALES"
+                        }
+                    ]);
+                response =
+                    Response.genQuickReply(i18n.__("directions.received"), [{
+                            title: i18n.__("curation.directions"),
+                            payload: "CLOSEST_DELI"
+                        },
+                        {
+                            title: i18n.__("curation.sales"),
+                            payload: "CARE_SALES"
+                        }
+                    ])
+                break;
+            case "CLOSEST_DELI":
+                response = Response.genPostbackButton(
+                    i18n.__("curation.show"),
+                    "CURATION_OTHER_STYLE"
+                )
+                break;
+            case "TRACK_ORDER":
+                response = Response.genQuickReply(i18n.__("order.prompt"), [{
+                        title: i18n.__("order.account"),
+                        payload: "LINK_ORDER"
+                    },
+                    {
+                        title: i18n.__("order.search"),
+                        payload: "SEARCH_ORDER"
+                    },
+                    {
+                        title: i18n.__("menu.help"),
+                        payload: "CARE_ORDER"
+                    }
+                ]);
+                break;
 
-      case "SEARCH_ORDER":
-        response = Response.genText(i18n.__("order.number"));
-        break;
+            case "SEARCH_ORDER":
+                response = Response.genText(i18n.__("order.number"));
+                break;
 
-      case "ORDER_NUMBER":
-        response = Response.genImageTemplate(
-          `${config.appUrl}/order.png`,
-          i18n.__("order.status")
-        );
-        break;
+            case "ORDER_NUMBER":
+                response = Response.genImageTemplate(
+                    `${config.appUrl}/order.png`,
+                    i18n.__("order.status")
+                );
+                break;
 
-      case "LINK_ORDER":
-        response = [
-          Response.genText(i18n.__("order.dialog")),
-          Response.genText(i18n.__("order.searching")),
-          Response.genImageTemplate(
-            `${config.appUrl}/order.png`,
-            i18n.__("order.status")
-          )
-        ];
-        break;
+            case "LINK_ORDER":
+                response = [
+                    Response.genText(i18n.__("order.dialog")),
+                    Response.genText(i18n.__("order.searching")),
+                    Response.genImageTemplate(
+                        `${config.appUrl}/order.png`,
+                        i18n.__("order.status")
+                    )
+                ];
+                break;
+        }
+
+        return response;
     }
-
-    return response;
-  }
 };
