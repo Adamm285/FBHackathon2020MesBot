541bddbffc783029c7ea389c8829465677c55596
Adam m <adam@floorsmtihllc.com>
2020-03-15 17:06:27 -0400

    testing payload on directions

--- services/directions.js
+++ services/directions.js
@@ -11,27 +11,28 @@ module.exports = class Directions {
         let response;
         console.log("switching to directions...", payload);
         switch (payload) {
-            case payload:
+            // case payload:
                 console.log("switched to directions...")
-                if (payload) {
-                      let delayed = {
-                        "text": `Great, I`
-                      }  
-                      setTimeout((data) => {
-                            console.log(delayed)
-                            return response 
-                        }, 4000);
-                      // res.status(200).send('Please close this window to return to the conversation thread.');
-                      // console.log("bring in ...", data)
-                      Recieve.callSendAPI(data, response);
-                }
-                break;
-                // 
-            case "CLOSEST_DELI":
+            //     if (payload) {
+            //           let delayed = {
+            //             "text": `Great, I`
+            //           }  
+            //           setTimeout((data) => {
+            //                 console.log(delayed)
+            //                 return response 
+            //             }, 4000);
+            //           // res.status(200).send('Please close this window to return to the conversation thread.');
+            //           // console.log("bring in ...", data)
+            //           Recieve.callSendAPI(data, response);
+            //     }
+            //     break;
+            //     // 
+            // case "CLOSEST_DELI":
                 response = Response.genPostbackButton(
                     i18n.__("curation.show"),
                     "CURATION_OTHER_STYLE"
                 )
+        }
                 break;
                 // 
         };
