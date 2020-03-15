52a09816af62f9d0f15b0daab4e30df51e9cebd5
Adam m <adam@floorsmtihllc.com>
2020-03-15 11:21:33 -0400

    adding .then()

--- services/directions.js
+++ services/directions.js
@@ -10,30 +10,23 @@ module.exports = class Order {
         let response;
         response = Response.genText(i18n.__("get_started.guidance"));
         console.log("switching to directions...", payload);
-        
+
         switch (response) {
             case response:
                 console.log("switched to directions...")
-                response =
-                    Response.genText(i18n.__("directions.received"), [{
-                            title: i18n.__("curation.directions"),
-                            payload: "CLOSEST_DELI"
-                        },
-                        {
-                            title: i18n.__("curation.sales"),
-                            payload: "CARE_SALES"
-                        }
-                    ]);
-                response =
-                    Response.genQuickReply(i18n.__("directions.received"), [{
-                            title: i18n.__("curation.directions"),
-                            payload: "CLOSEST_DELI"
-                        },
-                        {
-                            title: i18n.__("curation.sales"),
-                            payload: "CARE_SALES"
-                        }
-                    ])
+                response.then(function () {
+                    console.log("value"); // 1
+                    response =
+                        Response.genText(i18n.__("directions.received"), [{
+                                title: i18n.__("curation.directions"),
+                                payload: "CLOSEST_DELI"
+                            },
+                            {
+                                title: i18n.__("curation.sales"),
+                                payload: "CARE_SALES"
+                            }
+                        ]);
+                })
                 break;
             case "CLOSEST_DELI":
                 response = Response.genPostbackButton(
