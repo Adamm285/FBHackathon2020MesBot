f8c9e990ac167917b97288df335dcf4bab5a11cf
Adam m <adam@floorsmtihllc.com>
2020-03-15 16:16:58 -0400

    adding .then to curation 2\3

--- services/directions.js
+++ services/directions.js
@@ -22,53 +22,54 @@ module.exports = class Order {
                             payload: "CARE_SALES"
                         }
                     ]);
-                // })
+
                 break;
-        // 
-        case "CLOSEST_DELI":
-        response = Response.genPostbackButton(
-            i18n.__("curation.show"),
-            "CURATION_OTHER_STYLE"
-        )
-        break;
-        // 
-        case "TRACK_ORDER":
-        response = Response.genQuickReply(i18n.__("order.prompt"), [{
-                title: i18n.__("order.account"),
-                payload: "LINK_ORDER"
-            },
-            {
-                title: i18n.__("order.search"),
-                payload: "SEARCH_ORDER"
-            },
-            {
-                title: i18n.__("menu.help"),
-                payload: "CARE_ORDER"
-            }
-        ]);
-        break;
-        // 
-        case "SEARCH_ORDER":
-        response = Response.genText(i18n.__("order.number"));
-        break;
-        // 
-        case "ORDER_NUMBER":
-        response = Response.genImageTemplate(
-            `${config.appUrl}/order.png`,
-            i18n.__("order.status")
-        );
-        break;
-        // 
-        case "LINK_ORDER":
-        response = [
-            Response.genText(i18n.__("order.dialog")),
-            Response.genText(i18n.__("order.searching")),
-            Response.genImageTemplate(
-                `${config.appUrl}/order.png`,
-                i18n.__("order.status")
-            )
-        ];
-        break;
-    }
-    return response;
-}
+                // 
+            case "CLOSEST_DELI":
+                response = Response.genPostbackButton(
+                    i18n.__("curation.show"),
+                    "CURATION_OTHER_STYLE"
+                )
+                break;
+                // 
+            case "TRACK_ORDER":
+                response = Response.genQuickReply(i18n.__("order.prompt"), [{
+                        title: i18n.__("order.account"),
+                        payload: "LINK_ORDER"
+                    },
+                    {
+                        title: i18n.__("order.search"),
+                        payload: "SEARCH_ORDER"
+                    },
+                    {
+                        title: i18n.__("menu.help"),
+                        payload: "CARE_ORDER"
+                    }
+                ]);
+                break;
+                // 
+            case "SEARCH_ORDER":
+                response = Response.genText(i18n.__("order.number"));
+                break;
+                // 
+            case "ORDER_NUMBER":
+                response = Response.genImageTemplate(
+                    `${config.appUrl}/order.png`,
+                    i18n.__("order.status")
+                );
+                break;
+                // 
+            case "LINK_ORDER":
+                response = [
+                    Response.genText(i18n.__("order.dialog")),
+                    Response.genText(i18n.__("order.searching")),
+                    Response.genImageTemplate(
+                        `${config.appUrl}/order.png`,
+                        i18n.__("order.status")
+                    )
+                ];
+                break;
+        };
+        return response;
+    };
+};
