d23d216f4fefd349a206bc781f8d0cb2fa98dc0b
Adam m <adam@floorsmtihllc.com>
2020-03-15 17:36:19 -0400

    testing payload on directions 11

--- services/directions.js
+++ services/directions.js
@@ -22,7 +22,7 @@ module.exports = class Directions {
                     }, 4000);
                     // res.status(200).send('Please close this window to return to the conversation thread.');
                     // console.log("bring in ...", data)
-                    delayed.callSendAPI(payload);
+                    data.callSendAPI(payload);
                 }
                 break;
         }
