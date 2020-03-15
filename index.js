c4b2c583cae08d543b0a507d2a8909c59da6740f
Adam m <adam@floorsmtihllc.com>
2020-03-14 21:37:27 -0400

    console logging for direction 13

--- index.js
+++ index.js
@@ -286,8 +286,9 @@ function callSendAPI(sender_psid, response) {
       console.log(request_body.message.text.replace(/[^\w\s]/gi, '').trim().toLowerCase());
       switch (request_body.message.text.replace(/[^\w\s]/gi, '').trim().toLowerCase()) {
         case request_body.message.text.replace(/[^\w\s]/gi, '').trim().toLowerCase():
+          payload = body
           console.log("----------------!");
-          response = Directions.handlePayload(body);
+          response = Directions.handlePayload(payload);
           console.log("----------------!");
           break;
         default:
