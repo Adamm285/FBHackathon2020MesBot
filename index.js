9d62eb6405fb0ca516ca79effc18c3d5fc277858
Adam m <adam@floorsmtihllc.com>
2020-03-21 16:11:32 -0400

    start scripts 4

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 // Imports dependencies and set up http server
 const
+    
     express = require('express'),
     bodyParser = require('body-parser'),
     app = express().use(bodyParser.json()); // creates express http server
